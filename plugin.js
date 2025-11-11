e04944cdedf1494a874f12a3ebadc14acca429ae
Muril0B <116085436+Muril0B@users.noreply.github.com>
2025-11-11 19:16:16 -0300

    Refactor ControlAgent plugin with new features
    Updated plugin version and description. Enhanced startup function to include event dispatcher.

--- plugin.js
+++ plugin.js
@@ -1,17 +1,16 @@
 module.exports = {
   name: "ControlAgent",
-  version: "1.0",
-  description: "Ativa e desativa o agente remotamente pelo painel MeshCentral.",
-  startup: function (server) {
-    server.debug(1, "PLUGIN ControlAgent carregado com sucesso!");
-  },
-  handleServerCommand: function (server, ws, command) {
-    if (command.cmd === 'stop') {
-      server.debug(1, "Parando agente em: " + command.node);
-      server.SendCommand(command.node, { action: 'agentexec', type: 1, cmd: 'systemctl stop meshagent' });
-    } else if (command.cmd === 'start') {
-      server.debug(1, "Iniciando agente em: " + command.node);
-      server.SendCommand(command.node, { action: 'agentexec', type: 1, cmd: 'systemctl start meshagent' });
-    }
+  version: "1.0.0",
+  description: "Plugin para ativar e desativar agentes no MeshCentral.",
+  startup: function (server, args) {
+    console.log("PLUGIN ControlAgent INICIADO!");
+
+    // Exemplo de integração mínima
+    server.AddEventDispatcher({
+      action: "plugin-controlagent",
+      on: function (user, command) {
+        console.log(`Comando recebido de ${user.name}:`, command);
+      }
+    });
   }
 };
